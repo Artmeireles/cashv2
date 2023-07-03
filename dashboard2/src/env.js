d0259b0d42d5b77d7eb8b55f6cf49107d022dce6
Tom Mendes <contato@tommendes.com.br>
2023-07-03 18:06:54 -0300

    Dashboard
    Criação do dashboard baseado em sakay-vue

---
+++ dashboard2/src/env.js
@@ -0,0 +1,2 @@
+export const baseApiUrl = 'http://localhost:55595'; // dev
+export default { baseApiUrl };
---
+++ dashboard2/src/router/index.js
@@ -0,0 +1,205 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import AppLayout from '@/layout/AppLayout.vue';
+import { userKey } from '../global';
+
+const routes = [
+    {
+        path: '/',
+        component: AppLayout,
+        children: [
+            {
+                path: '/',
+                name: 'dashboard',
+                component: () => import('@/views/Dashboard.vue')
+            },
+            {
+                path: '/uikit/formlayout',
+                name: 'formlayout',
+                component: () => import('@/views/uikit/FormLayout.vue')
+            },
+            {
+                path: '/uikit/input',
+                name: 'input',
+                component: () => import('@/views/uikit/Input.vue')
+            },
+            {
+                path: '/uikit/floatlabel',
+                name: 'floatlabel',
+                component: () => import('@/views/uikit/FloatLabel.vue')
+            },
+            {
+                path: '/uikit/invalidstate',
+                name: 'invalidstate',
+                component: () => import('@/views/uikit/InvalidState.vue')
+            },
+            {
+                path: '/uikit/button',
+                name: 'button',
+                component: () => import('@/views/uikit/Button.vue')
+            },
+            {
+                path: '/uikit/table',
+                name: 'table',
+                component: () => import('@/views/uikit/Table.vue')
+            },
+            {
+                path: '/uikit/list',
+                name: 'list',
+                component: () => import('@/views/uikit/List.vue')
+            },
+            {
+                path: '/uikit/tree',
+                name: 'tree',
+                component: () => import('@/views/uikit/Tree.vue')
+            },
+            {
+                path: '/uikit/panel',
+                name: 'panel',
+                component: () => import('@/views/uikit/Panels.vue')
+            },
+
+            {
+                path: '/uikit/overlay',
+                name: 'overlay',
+                component: () => import('@/views/uikit/Overlay.vue')
+            },
+            {
+                path: '/uikit/media',
+                name: 'media',
+                component: () => import('@/views/uikit/Media.vue')
+            },
+            {
+                path: '/uikit/menu',
+                component: () => import('@/views/uikit/Menu.vue'),
+                children: [
+                    {
+                        path: '/uikit/menu',
+                        component: () => import('@/views/uikit/menu/PersonalDemo.vue')
+                    },
+                    {
+                        path: '/uikit/menu/seat',
+                        component: () => import('@/views/uikit/menu/SeatDemo.vue')
+                    },
+                    {
+                        path: '/uikit/menu/payment',
+                        component: () => import('@/views/uikit/menu/PaymentDemo.vue')
+                    },
+                    {
+                        path: '/uikit/menu/confirmation',
+                        component: () => import('@/views/uikit/menu/ConfirmationDemo.vue')
+                    }
+                ]
+            },
+            {
+                path: '/uikit/message',
+                name: 'message',
+                component: () => import('@/views/uikit/Messages.vue')
+            },
+            {
+                path: '/uikit/file',
+                name: 'file',
+                component: () => import('@/views/uikit/File.vue')
+            },
+            {
+                path: '/uikit/charts',
+                name: 'charts',
+                component: () => import('@/views/uikit/Chart.vue')
+            },
+            {
+                path: '/uikit/misc',
+                name: 'misc',
+                component: () => import('@/views/uikit/Misc.vue')
+            },
+            {
+                path: '/blocks',
+                name: 'blocks',
+                component: () => import('@/views/utilities/Blocks.vue')
+            },
+            {
+                path: '/utilities/icons',
+                name: 'icons',
+                component: () => import('@/views/utilities/Icons.vue')
+            },
+            {
+                path: '/pages/timeline',
+                name: 'timeline',
+                component: () => import('@/views/pages/Timeline.vue')
+            },
+            {
+                path: '/pages/empty',
+                name: 'empty',
+                component: () => import('@/views/pages/Empty.vue')
+            },
+            {
+                path: '/pages/crud',
+                name: 'crud',
+                component: () => import('@/views/pages/Crud.vue')
+            },
+            {
+                path: '/documentation',
+                name: 'documentation',
+                component: () => import('@/views/utilities/Documentation.vue')
+            }
+        ]
+    },
+    {
+        path: '/welcome',
+        name: 'welcome',
+        component: () => import('@/views/pages/Home.vue')
+    },
+    {
+        path: '/not-found',
+        name: 'notfound',
+        component: () => import('@/views/pages/NotFound.vue')
+    },
+    {
+        path: '/signin',
+        name: 'signin',
+        component: () => import('@/views/pages/auth/SignIn.vue')
+    },
+    {
+        path: '/signup',
+        name: 'signup',
+        component: () => import('@/views/pages/auth/SignUp.vue')
+    },
+    {
+        path: '/password-reset',
+        name: 'password-reset',
+        component: () => import('@/views/pages/auth/UserPassReset.vue')
+    },
+    {
+        path: '/request-password-reset',
+        name: 'request-password-reset',
+        component: () => import('@/views/pages/auth/UserRequestPassReset.vue')
+    },
+    {
+        path: '/u-token',
+        name: 'u-token',
+        component: () => import('@/views/pages/auth/UserToken.vue')
+    },
+];
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: routes
+});
+
+router.beforeEach((to, from, next) => {
+    const nameUnblockedRoutes = ['welcome', 'signin', 'signup', 'u-token', 'not-found', 'request-password-reset', 'password-reset'];
+    const json = localStorage.getItem(userKey);
+    const user = JSON.parse(json);
+    const paths = [];
+    routes.forEach((element) => {
+        // console.log(element.path);
+        paths.push(element.path);
+    });
+    // console.log(to.path, paths.includes(to.path));
+    // next();
+    if (!paths.includes(to.path)) next({ path: '/not-found' });
+    else if (user && user.id && to.path == '/signin') next({ path: '/' });
+    else {
+        if (!nameUnblockedRoutes.includes(to.name) && !(user && user.id)) next({ path: '/welcome' });
+        else next();
+    }
+});
+
+export default router;
