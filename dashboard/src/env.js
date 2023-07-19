ca8025975de03d04ab564c15ca159b84c2d32bc6
Tom Mendes <contato@tommendes.com.br>
2023-07-19 15:29:22 -0300

    API
    Mudanças na recepção dos dados de dependentes, servidores, vinculos e remunerações do PonteCashV2

--- dashboard/src/env.js
+++ dashboard/src/env.js
@@ -1,5 +1,5 @@
-export const baseApiAuthUrl = 'http://localhost:55595'; // dev
-// export const baseApiUrl = 'https://api.mgcash.app.br'; // prod
-// export const baseApiUrl = 'http://localhost:55595'; // dev
-export const baseApiUrl = 'http://localhost:3000'; // dev
+// export const baseApiAuthUrl = 'http://localhost:55595'; // dev
+// export const baseApiUrl = 'http://localhost:3000'; // dev
+export const baseApiAuthUrl = 'https://api.mgcash.app.br'; // prod
+export const baseApiUrl = 'https://api.mgcash.app.br'; // prod
 export default { baseApiUrl };
