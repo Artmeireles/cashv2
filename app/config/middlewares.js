e2ee6832a71f26d328dbabeea9038163c94885fe
Tom Mendes <contato@tommendes.com.br>
2025-04-01 17:59:43 -0300

    Payments
    Nova interface de folhas a pagar. PrimeVue 4. Vue 3

--- app/config/middlewares.js
+++ app/config/middlewares.js
@@ -1,9 +1,7 @@
 const bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload')
 
 module.exports = app => {
     app.use(bodyParser.json({ limit: '10mb', extended: true }))
     app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
     app.use(bodyParser.text({ limit: '10mb', extended: true }));
-    app.use(fileUpload())
 }
---
+++ paymentss/src/App.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <span>Hello Payments</span>
+</template>
+
+<style scoped></style>
